846315bdccff1768db6e1b3fce16279f0ae6b07d
walkermannum1 <2469377884@qq.com>
2016-06-12 17:32:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/service/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.user.service;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by user on 2016/6/12.
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button startService;
+    private Button stopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this,MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this,MyService.class);
+                stopService(stopIntent);
+                break;
+            default:
+                break;
+        }
+    }
+}
