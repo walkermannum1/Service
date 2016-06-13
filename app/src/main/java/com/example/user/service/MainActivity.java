32f3e9ee0eff45279bc2c010659c62ca10fde9b0
walkermannum1 <2469377884@qq.com>
2016-06-13 17:07:56 +0800

    Bind service.

--- app/src/main/java/com/example/user/service/MainActivity.java
+++ app/src/main/java/com/example/user/service/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.service;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +16,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button startService;
     private Button stopService;
+    private Button bindService;
+    private Button unbindService;
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         stopService = (Button) findViewById(R.id.stop_service);
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -33,6 +57,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this,MyService.class);
                 stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
                 break;
             default:
                 break;
--- app/src/main/java/com/example/user/service/MyService.java
+++ app/src/main/java/com/example/user/service/MyService.java
@@ -3,14 +3,27 @@ package com.example.user.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
 
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("Myservice","startDownload executed");
+        }
+        public int getProgress() {
+            Log.d("Myservice","getProgress executed");
+            return 0;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
     @Override
     public void onCreate() {
