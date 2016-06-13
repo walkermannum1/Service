e0867643ccff20dd2685ce2c588b16942d87f2df
walkermannum1 <2469377884@qq.com>
2016-06-13 16:29:13 +0800

    Add log

--- app/src/main/java/com/example/user/service/MyService.java
+++ app/src/main/java/com/example/user/service/MyService.java
@@ -4,6 +4,7 @@ package com.example.user.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
 
@@ -14,14 +15,17 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("Myservice","onCreate executed");
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService","onStartCommand executed");
         return super.onStartCommand(intent,flags,startId);
     }
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MyService","onDestory executed");
     }
 
 }
