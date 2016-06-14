e69d28bdea61339927d97afb7fdfa9a9653c34af
walkermannum1 <2469377884@qq.com>
2016-06-14 17:24:56 +0800

    Fix notification.

--- app/src/main/java/com/example/user/service/MyService.java
+++ app/src/main/java/com/example/user/service/MyService.java
@@ -1,10 +1,15 @@
 package com.example.user.service;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 
 public class MyService extends Service {
@@ -28,6 +33,21 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(MyService.this);
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setContentTitle("This is ticker text");
+        builder.setContentText("Content text");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setDefaults(Notification.DEFAULT_ALL);
+        builder.setAutoCancel(true);
+        Intent intent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        // builder.setVibrate (new long[] {0,2000,3000,4000} );
+        Notification notification = builder.build();
+        manager.notify(1,notification);
+        startForeground(1,notification);
         Log.d("Myservice","onCreate executed");
     }
     @Override
---
+++ app/src/main/java/com/example/user/service/NotificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.service;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 2016/5/30.
+ */
+public class NotificationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_layout);
+    }
+}
